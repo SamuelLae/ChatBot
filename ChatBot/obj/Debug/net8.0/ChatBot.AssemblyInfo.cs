808a71f40cf61691d9c042ae473af90e42a03cf2
samuel <samuel.laestander@edu.lycksele.se>
2025-04-25 13:24:14 +0200

    Test 5

--- ChatBot/obj/Debug/net8.0/ChatBot.AssemblyInfo.cs
+++ ChatBot/obj/Debug/net8.0/ChatBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e425b07e5160a9cb211ef07755cc1941ca03e63d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+266d92ac8849928ad0c9bf81244bb5b7612fed8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
